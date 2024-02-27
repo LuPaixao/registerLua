830a2e7c82b2bff26592e38e1801913f4c6a1a41
Luana Albuquerque Moreira <luanapaixao.am@gmail.com>
2024-02-27 17:37:45 -0300

    inicio_programa

---
+++ src/test/java/register/lua/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package register.lua.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
